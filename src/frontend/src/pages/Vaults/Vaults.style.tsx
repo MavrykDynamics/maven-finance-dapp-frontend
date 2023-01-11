bc8df35fff1c580fc25815b2d19c525b9c89caa4
Evgeniy Polishchuk <68184643+CasualJackie@users.noreply.github.com>
2023-01-11 17:41:52 +0200

    added dropdowns

--- src/frontend/src/pages/Vaults/Vaults.style.tsx
+++ src/frontend/src/pages/Vaults/Vaults.style.tsx
@@ -11,5 +11,25 @@ export const VaultsStyled = styled.div<{ theme: MavrykTheme }>`
 `
 
 export const VaultsSearchFilterStyled = styled(Card)`
-  
+  display: flex;
+
+  input {
+    width: 320px;
+  }
+
+  .dd-container {
+    display: grid;
+    grid-template-columns: 70px 180px 180px 180px;
+    column-gap: 20px;
+
+    h4 {
+      margin: 0;
+    }
+
+    .dd-item {
+      div {
+        width: 180px;
+      }
+    }
+  }
 `
--- src/frontend/src/pages/Vaults/components/VaultsSearchFilter.view.tsx
+++ src/frontend/src/pages/Vaults/components/VaultsSearchFilter.view.tsx
@@ -1,10 +1,16 @@
-import * as React from 'react'
+import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 // components
+import { Input } from 'app/App.components/Input/Input.controller'
+import { DropdownContainer } from 'app/App.components/DropDown/DropDown.style'
+import { DropDown } from 'app/App.components/DropDown/DropDown.controller'
 
 // styles
 import { VaultsSearchFilterStyled } from './../Vaults.style'
+
+// helpers
+import { sortByCategory } from 'utils/sortByCategory'
 
 // types
 import { State } from '../../../reducers'
@@ -13,9 +19,81 @@ export const VaultsSearchFilter = () => {
   const dispatch = useDispatch()
   const { wallet, tezos, accountPkh } = useSelector((state: State) => state.wallet)
 
+  const [searchInputValue, setSearchInput] = useState('')
+  const [sortedData, setSortedData] = useState<unknown[]>([])
+  const [ddIsOpen, setDdIsOpen] = useState(false)
+  const [chosenDdItem, setChosenDdItem] = useState<string | undefined>()
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchQuery = e.target.value
+  
+    let searchResult: unknown[] = []
+
+    if (searchQuery !== '') {
+      searchResult = []
+    } else {
+      searchResult = []
+    }
+
+    setSearchInput(e.target.value)
+    setSortedData(searchResult)
+  }
+
+  const handleSelect = (selectedOption: string) => {
+    setDdIsOpen(!ddIsOpen)
+    setChosenDdItem(selectedOption)
+
+    if (selectedOption !== '' && selectedOption !== chosenDdItem) {
+      setSortedData((data: unknown[]) => {
+        // return sortByCategory(data, selectedOption)
+        return data
+      })
+    }
+  }
+
   return (
     <VaultsSearchFilterStyled>
-      
+      <Input
+        type="text"
+        kind={'search'}
+        placeholder="Search data feed..."
+        onChange={handleSearch}
+        value={searchInputValue}
+      />
+
+      <DropdownContainer className="dd-container">
+        <h4>Order by:</h4>
+
+        <DropDown
+          className="dd-item"
+          placeholder="Statuses"
+          isOpen={ddIsOpen}
+          setIsOpen={setDdIsOpen}
+          itemSelected={chosenDdItem}
+          items={[]}
+          clickOnItem={handleSelect}
+        />
+
+        <DropDown
+          className="dd-item"
+          placeholder="All Sizes"
+          isOpen={ddIsOpen}
+          setIsOpen={setDdIsOpen}
+          itemSelected={chosenDdItem}
+          items={[]}
+          clickOnItem={handleSelect}
+        />
+
+        <DropDown
+          className="dd-item"
+          placeholder="All Assets"
+          isOpen={ddIsOpen}
+          setIsOpen={setDdIsOpen}
+          itemSelected={chosenDdItem}
+          items={[]}
+          clickOnItem={handleSelect}
+        />
+      </DropdownContainer>
     </VaultsSearchFilterStyled>
   )
 }
