f583b2b0c0b276b671f89d138eb5c9bdac812e94
Evgeniy Polishchuk <68184643+CasualJackie@users.noreply.github.com>
2023-01-24 10:07:40 +0200

    added title, text and closeModal icon

--- src/frontend/src/pages/Vaults/components/LiquidateVaultModal/LiquidateVaultModal.styles.tsx
+++ src/frontend/src/pages/Vaults/components/LiquidateVaultModal/LiquidateVaultModal.styles.tsx
@@ -2,5 +2,41 @@ import styled from "styled-components";
 import { MavrykTheme } from "styles/interfaces";
 
 export const LiquidateVaultModalStyled = styled.div<{ theme: MavrykTheme }>`
+  & > h1 {
+    margin: 0;
+  }
 
+  & > p {
+    margin: 0;
+    margin-bottom: 30px;
+  }
+
+  .close-modal {
+    position: absolute;
+    top: 15px;
+    right: 15px;
+    cursor: pointer;
+    transition: opacity 0.3s;
+
+    &:after,
+    &:before {
+      content: '';
+      height: 30px;
+      width: 30px;
+      border-top: 3px solid ${({ theme }) => theme.valueColor};
+      position: absolute;
+      top: 10px;
+      right: -12px;
+      transform: rotate(-45deg);
+    }
+
+    &:before {
+      right: 9px;
+      transform: rotate(45deg);
+    }
+
+    &:hover {
+      opacity: 0.7;
+    }
+  }
 `
--- src/frontend/src/pages/Vaults/components/LiquidateVaultModal/LiquidateVaultModal.view.tsx
+++ src/frontend/src/pages/Vaults/components/LiquidateVaultModal/LiquidateVaultModal.view.tsx
@@ -14,7 +14,16 @@ export const LiquidateVaultModal = ({ closePopup, show }: Props) => {
     <PopupContainer onClick={closePopup} show={show}>
       <PopupContainerWrapper onClick={(e) => e.stopPropagation()} className="loans">
         <LiquidateVaultModalStyled>
-          Hello moto
+          <button onClick={closePopup} className="close-modal" />
+          <h1>Liquidate Vault</h1>
+          <p>
+            Foreclosing (liquidating) a vault repays the vault’s debt, by purchasing the vault’s collateral.
+            Liquidators earn an additional 10% yield on top of the debt repaid for helping to secure Mavryk’s lending.
+            Foreclosing on a vault requires repaying the vault debt in the same asset.
+            The most that can be liquidated from a vault is 50%.
+            Input a percentage and then review your liquidation details below.
+          </p>
+
         </LiquidateVaultModalStyled>
       </PopupContainerWrapper>
     </PopupContainer>
