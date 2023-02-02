f311ed04660f3a3c8fdb7c2c4e29f7d1dbc48823
Evgeniy Polishchuk <68184643+CasualJackie@users.noreply.github.com>
2023-02-02 13:54:01 +0200

    fixed conversion bytes to string

--- src/frontend/src/utils/bytesToString.ts
+++ src/frontend/src/utils/bytesToString.ts
@@ -1,6 +1,22 @@
-import { bytes2Char } from "@taquito/utils";
+import { MichelsonType, unpackDataBytes } from "@taquito/michel-codec";
 
-export const bytesToString = (bytes: string) => {
-  const slicedBytes = bytes.slice(12)
-  return slicedBytes ?  bytes2Char(slicedBytes) : ''
+export const BYTES_ADDRESS_TYPE = 'address'
+export const BYTES_STRING_TYPE = 'string'
+
+const stringType: MichelsonType = {
+  prim: 'string',
+};
+
+const addressType: MichelsonType = {
+  prim: 'address',
+};
+
+export type BytesType = typeof BYTES_ADDRESS_TYPE | typeof BYTES_STRING_TYPE
+
+export const bytesToString = (bytes: string, type?: BytesType) => {
+  const bytesData = { bytes }
+  const bytesType = type === BYTES_ADDRESS_TYPE ? addressType : stringType
+  
+  const data = unpackDataBytes(bytesData, bytesType) as { string: string }
+  return data.string
 }
