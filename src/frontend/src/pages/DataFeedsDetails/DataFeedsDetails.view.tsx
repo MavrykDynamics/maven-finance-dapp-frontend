d819a82153a12c7ef35d0da94dde419153fc95b3
Kucher Maxym <max.kucher98@gmail.com>
2023-02-16 18:02:05 +0200

    remove static icons

--- src/frontend/src/pages/DataFeedsDetails/DataFeedsDetails.view.tsx
+++ src/frontend/src/pages/DataFeedsDetails/DataFeedsDetails.view.tsx
@@ -73,12 +73,7 @@ const DataFeedDetailsView = ({ feed, feedsSatellites, registerFeedHandler, isLoa
 
   const isTrustedAnswer = feed && feed.last_completed_data_pct_oracle_resp >= feed.pct_oracle_threshold
 
-  // TODO: temporary icon selection cuz of troubles with usage from indexer
-  const imageLink = feed?.name.includes('EUROC')
-    ? '/images/eurl.png'
-    : feed?.name.includes('XTZ')
-    ? '/images/tezos.png'
-    : dipDupContracts.find(({ contract }) => contract === feed?.address)?.metadata?.icon
+  const imageLink = dipDupContracts.find(({ contract }) => contract === feed?.address)?.metadata?.icon
 
   return feed ? (
     <Page>
