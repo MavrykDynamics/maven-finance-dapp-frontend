76e0408761c24899918870d628b4aff8f790a1d7
Evgeniy Polishchuk <68184643+CasualJackie@users.noreply.github.com>
2023-03-14 19:01:46 +0200

    updated  footer of side menu

--- src/frontend/src/app/App.components/Menu/Menu.style.tsx
+++ src/frontend/src/app/App.components/Menu/Menu.style.tsx
@@ -139,14 +139,22 @@ export const MenuGrid = styled.div`
 `
 
 export const MenuFooter = styled.div<{ theme: MavrykTheme }>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   font-size: 11px;
   color: ${({ theme }) => theme.footerColor};
   font-weight: 600;
   padding: 0 14px;
+  row-gap: 20px;
+
+  span {
+    margin-bottom: 20px;
+    color: ${({ theme }) => theme.valueColor};
+  }  
 
   .social-wrapper {
     display: flex;
-    margin-bottom: 10px;
     column-gap: 5px;
     a {
       width: 30px;
@@ -167,11 +175,6 @@ export const MenuFooter = styled.div<{ theme: MavrykTheme }>`
   @media screen and (max-width: 1460px) {
     padding: 0 10px;
     font-size: 10px;
-  }
-
-  > p {
-    display: inline-block;
-    font-weight: 500;
   }
 
   &.menu-collapsed {
