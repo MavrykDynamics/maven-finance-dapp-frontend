deb0712b4bba2c0f9cad2b923f77963f90d50437
kucher-mx <max.kucher98@gmail.com>
2023-04-18 17:00:02 +0300

    fix my lending

--- src/frontend/src/pages/Dashboard/Dashboard.controller.tsx
+++ src/frontend/src/pages/Dashboard/Dashboard.controller.tsx
@@ -30,6 +30,7 @@ export const Dashboard = () => {
   } = useSelector((state: State) => state.doorman)
   const { treasuryStorage, isLoaded: isTreasuryLoaded } = useSelector((state: State) => state.treasury)
   const { isLoaded: isVestingLoaded } = useSelector((state: State) => state.vesting)
+  const { accountPkh } = useSelector((state: State) => state.wallet)
   const { isGovernanceStorageLoaded } = useSelector((state: State) => state.governance)
   const {
     vaultsList: { allVaultsIds, vaultsMapper },
@@ -81,24 +82,21 @@ export const Dashboard = () => {
 
   const tvlValue = doormanTVL + treasuryTVL + farmsTVL + lendingTvl + vaultsTvl
 
-  const { isLoading } = useDataLoader(
-    async (isDepsChanged) => {
-      try {
-        await Promise.all(
-          [
-            (!isGovernanceStorageLoaded || isDepsChanged) && dispatch(getGovernanceStorage()),
-            (!isVaultsLoaded || isDepsChanged) && dispatch(getVaultsStorage()),
-            (!isVestingLoaded || isDepsChanged) && dispatch(getVestingStorage()),
-            (!isTreasuryLoaded || isDepsChanged) && dispatch(fillTreasuryStorage()),
-            (!isLoansLoaded || isDepsChanged) && dispatch(getLoansStorage()),
-            (!isFarmsLoaded || isDepsChanged) && dispatch(getFarmStorage()),
-            (!isDoormanLoaded || isDepsChanged) && dispatch(getDoormanStorage()),
-          ].filter(Boolean),
-        )
-      } catch (e) {}
-    },
-    [isLoansLoaded],
-  )
+  const { isLoading } = useDataLoader(async (isDepsChanged) => {
+    try {
+      await Promise.all(
+        [
+          (!isGovernanceStorageLoaded || isDepsChanged) && dispatch(getGovernanceStorage()),
+          (!isVaultsLoaded || isDepsChanged) && dispatch(getVaultsStorage()),
+          (!isVestingLoaded || isDepsChanged) && dispatch(getVestingStorage()),
+          (!isTreasuryLoaded || isDepsChanged) && dispatch(fillTreasuryStorage()),
+          (!isLoansLoaded || isDepsChanged) && dispatch(getLoansStorage()),
+          (!isFarmsLoaded || isDepsChanged) && dispatch(getFarmStorage()),
+          (!isDoormanLoaded || isDepsChanged) && dispatch(getDoormanStorage()),
+        ].filter(Boolean),
+      )
+    } catch (e) {}
+  }, [])
 
   const mvkStatsBlock: mvkStatsType = {
     marketCap: marketCapValue,
