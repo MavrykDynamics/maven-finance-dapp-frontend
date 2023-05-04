043e4291d97aa1589c2115db9440fbd1aca6a6c6
alexbelz123 <belz.alexx@gmail.com>
2023-05-04 19:22:53 +0300

    MAV-1535/fixed borrowed & landing rows

--- src/frontend/src/pages/Loans/Loans.controller.tsx
+++ src/frontend/src/pages/Loans/Loans.controller.tsx
@@ -203,8 +203,8 @@ export const Loans = () => {
                     <div className="row">
                       <ThreeLevelListItem>
                         <div className="name">Total Lending</div>
-                        <CommaNumber beginningText="$" value={totalLended} className="value" />
-                        {rate ? <CommaNumber value={totalLended * rate} className="rate" /> : null}
+                        {rate ? <CommaNumber beginningText="$" value={totalLended * rate} className="value" /> : null}
+                        <CommaNumber value={totalLended} className="rate" />
                       </ThreeLevelListItem>
                       <ThreeLevelListItem>
                         <div className="name">Lend APY</div>
@@ -237,8 +237,8 @@ export const Loans = () => {
                     <div className="row">
                       <ThreeLevelListItem>
                         <div className="name">Total Borrowed</div>
-                        <CommaNumber beginningText="$" value={totalBorrowed} className="value" />
-                        {rate ? <CommaNumber value={totalBorrowed * rate} className="rate" /> : null}
+                        {rate ? <CommaNumber beginningText="$" value={totalBorrowed * rate} className="value" /> : null}
+                        <CommaNumber value={totalBorrowed} className="rate" />
                       </ThreeLevelListItem>
                       <ThreeLevelListItem>
                         <div className="name">Borrow APR</div>
