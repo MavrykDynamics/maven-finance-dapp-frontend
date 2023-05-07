c878181cbd67cdbc606a6f41180a8511cfd7033f
kucher-mx <max.kucher98@gmail.com>
2023-05-07 15:41:22 +0300

    fixed sidebar issue

--- src/frontend/src/app/App.style.tsx
+++ src/frontend/src/app/App.style.tsx
@@ -9,7 +9,7 @@ export const AppStyled = styled.div<{ theme: MavrykTheme; isExpandedMenu?: boole
   min-height: 100vh;
   padding-left: ${({ isExpandedMenu }) => (isExpandedMenu ? '232px' : '72px')};
 
-  @media screen and (max-width: 1400px) {
+  @media screen and (max-width: 1399px) {
     padding-left: 72px;
   }
 `
--- src/frontend/src/gql/fetchGraphQL.ts
+++ src/frontend/src/gql/fetchGraphQL.ts
@@ -1,4 +1,4 @@
-import { fetchGraphQL } from 'utils/fetchers/fetchGraphQL'
+import { fetchGraphQL } from 'utils/Fetchers/fetchGraphQL'
 
 export async function fetchFromIndexer(
   operationsDoc: string,
