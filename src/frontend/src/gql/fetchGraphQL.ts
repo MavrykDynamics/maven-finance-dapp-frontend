6739dd56ca9323820d8cfd91fb7dc140dfe721a4
kucher-mx <max.kucher98@gmail.com>
2023-05-09 21:26:03 +0300

    updated path to fix build

--- src/frontend/src/gql/fetchGraphQL.ts
+++ src/frontend/src/gql/fetchGraphQL.ts
@@ -1,4 +1,4 @@
-import { fetchGraphQL } from 'utils/Fetchers/fetchGraphQL'
+import { fetchGraphQL } from '../utils/Fetchers/fetchGraphQL'
 
 export async function fetchFromIndexer(
   operationsDoc: string,
