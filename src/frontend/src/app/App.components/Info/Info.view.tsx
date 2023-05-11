522e2c9680e83c2f5be2a74e6a7ea1a929cfeb05
alexbelz123 <belz.alexx@gmail.com>
2023-05-11 19:22:38 +0300

    fixed conflict

--- src/frontend/src/app/App.components/Info/Info.view.tsx
+++ src/frontend/src/app/App.components/Info/Info.view.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames'
 import Icon from '../Icon/Icon.view'
 import { getIconForInfoTyType, infoType } from './info.constants'
 import { InfoBlock } from './info.style'
@@ -5,6 +6,8 @@ import { InfoBlock } from './info.style'
 type Props = {
   text: string | JSX.Element
   type: infoType
+  showIcon?: boolean
+  isLarge?: boolean
   children?: React.ReactNode
 }
 
@@ -17,20 +20,26 @@ type Props = {
  * text: simple string or JSX.Element (you should use JSX when there is a need to add some colored text, timer etc.
  * NOTE: no images, buttons and other similar stuff, only text)
  * type: 'info' | 'error'
- * child: React.ReactNode - element that is displayed on the right side of the banner (f.e. timer, button)
+ * isLarge: boolean value to take large className for banner
+ * showIcon: boolean value to show left banner icon
+ * children: React.ReactNode - element that is displayed on the right side of the banner (f.e. timer, button)
  * }
  * @returns
  */
-export const Info = ({ children, text, type }: Props) => {
+export const Info = ({ children, text, type, showIcon = true, isLarge = false }: Props) => {
   const iconToUse = getIconForInfoTyType(type)
 
+  const bannerClasses = classNames(type, {
+    isLarge,
+    hasBorder: showIcon,
+    hasChild: Boolean(children),
+  })
+
   return (
-    <InfoBlock className={type}>
-      <div className="content">
-        <Icon id={iconToUse} className="info-icon" />
-        <p>{text}</p>
-        <div className="child">{children}</div>
-      </div>
+    <InfoBlock className={bannerClasses}>
+      {showIcon ? <Icon id={iconToUse} className="info-icon" /> : null}
+      <p>{text}</p>
+      {children ? <div className="child">{children}</div> : null}
     </InfoBlock>
   )
 }
