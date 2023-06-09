3b73d031a352a9d3beab83f5b44c4e038b28e1f7
kucher-mx <max.kucher98@gmail.com>
2023-06-09 11:16:00 +0300

    MAV-1491 #time 6m   fixed api error

--- src/frontend/src/utils/api/api-helpers/getTimestampByLevel.ts
+++ src/frontend/src/utils/api/api-helpers/getTimestampByLevel.ts
@@ -2,8 +2,7 @@ import { z } from 'zod'
 
 export const getTimestampByLevelSchema = z.string()
 
-export const getTimestampByLevelUrl = (level: number) =>
-  `https://apiWithAbort.ghostnet.tzkt.io/v1/blocks/${level}/timestamp`
+export const getTimestampByLevelUrl = (level: number) => `https://api.ghostnet.tzkt.io/v1/blocks/${level}/timestamp`
 
 export const getTimestampByLevelHeaders = {
   'Content-type': 'application/json',
