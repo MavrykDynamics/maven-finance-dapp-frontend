58c8b9e071b2397a4eaf6c414523c9777556950a
Sam Morrow <smorrow32@gmail.com>
2023-06-21 16:16:31 +0300

    MAV-000 #time   /Adding button + functionality for a user to go and close all of their empty vaults

--- src/frontend/src/pages/Admin/Admin.controller.tsx
+++ src/frontend/src/pages/Admin/Admin.controller.tsx
@@ -1,62 +1,67 @@
 import * as React from 'react'
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { State } from '../../reducers'
+import {useEffect} from 'react'
+import {useDispatch} from 'react-redux'
 import {
   addAllCollateralTokensToMarkets,
   addAllLoanTokensToMarkets,
   adminChangeGovernancePeriod,
   ChangeAllAdminsFromGovernance,
+  closeAllOfUsersEmptyVaults,
   createFarm,
   createTreasuries,
 } from './Admin.actions'
-import { Page } from 'styles'
-import { PageHeader } from '../../app/App.components/PageHeader/PageHeader.controller'
-import { AdminView } from './Admin.view'
-import { getLoansStorage } from '../Loans/Actions/getLoansData.actions'
-import { getGovernanceStorage } from 'pages/Governance/actions/GovernanseData.actions'
+import {Page} from 'styles'
+import {PageHeader} from '../../app/App.components/PageHeader/PageHeader.controller'
+import {AdminView} from './Admin.view'
+import {getLoansStorage} from '../Loans/Actions/getLoansData.actions'
+import {getGovernanceStorage} from 'pages/Governance/actions/GovernanseData.actions'
 
 export const Admin = () => {
-  const dispatch = useDispatch()
+    const dispatch = useDispatch()
 
-  useEffect(() => {
-    dispatch(getLoansStorage())
-    dispatch(getGovernanceStorage())
-  }, [dispatch])
+    useEffect(() => {
+        dispatch(getLoansStorage())
+        dispatch(getGovernanceStorage())
+    }, [dispatch])
 
-  const handleChangeGovernancePeriod = (chosenPeriod: string) => {
-    dispatch(adminChangeGovernancePeriod(chosenPeriod))
-  }
+    const handleChangeGovernancePeriod = (chosenPeriod: string) => {
+        dispatch(adminChangeGovernancePeriod(chosenPeriod))
+    }
 
-  const handleCreateFarm = () => {
-    dispatch(createFarm())
-  }
+    const handleCreateFarm = () => {
+        dispatch(createFarm())
+    }
 
-  const handleAddAllCollateralTokensToLendBorrow = () => {
-    console.log('Here in add all collateral tokens')
-    dispatch(addAllCollateralTokensToMarkets())
-  }
-  const handleAddAllLoanTokensToLendBorrow = () => {
-    console.log('Here in add all loan tokens')
-    dispatch(addAllLoanTokensToMarkets())
-  }
-  const ChangeAllAdminsFromGov = () => {
-    dispatch(ChangeAllAdminsFromGovernance())
-  }
-  const handleCreateAllTreasuries = () => {
-    dispatch(createTreasuries())
-  }
-  return (
-    <Page>
-      <PageHeader page={'admin'} />
-      <AdminView
-        handleChangeGovernancePeriod={handleChangeGovernancePeriod}
-        handleCreateFarm={handleCreateFarm}
-        handleChangeAllAdminsFromGov={ChangeAllAdminsFromGov}
-        handleAddAllLoanTokensToLendBorrow={handleAddAllLoanTokensToLendBorrow}
-        handleAddAllCollateralTokensToLendBorrow={handleAddAllCollateralTokensToLendBorrow}
-        handleCreateAllTreasuries={handleCreateAllTreasuries}
-      />
-    </Page>
-  )
+    const handleAddAllCollateralTokensToLendBorrow = () => {
+        console.log('Here in add all collateral tokens')
+        dispatch(addAllCollateralTokensToMarkets())
+    }
+    const handleAddAllLoanTokensToLendBorrow = () => {
+        console.log('Here in add all loan tokens')
+        dispatch(addAllLoanTokensToMarkets())
+    }
+    const ChangeAllAdminsFromGov = () => {
+        dispatch(ChangeAllAdminsFromGovernance())
+    }
+    const handleCreateAllTreasuries = () => {
+        dispatch(createTreasuries())
+    }
+
+    const handleCloseUsersEmptyVaults = () => {
+        dispatch(closeAllOfUsersEmptyVaults())
+    }
+    return (
+        <Page>
+            <PageHeader page={'admin'}/>
+            <AdminView
+                handleChangeGovernancePeriod={handleChangeGovernancePeriod}
+                handleCreateFarm={handleCreateFarm}
+                handleChangeAllAdminsFromGov={ChangeAllAdminsFromGov}
+                handleAddAllLoanTokensToLendBorrow={handleAddAllLoanTokensToLendBorrow}
+                handleAddAllCollateralTokensToLendBorrow={handleAddAllCollateralTokensToLendBorrow}
+                handleCreateAllTreasuries={handleCreateAllTreasuries}
+                handleCloseUsersEmptyVaults={handleCloseUsersEmptyVaults}
+            />
+        </Page>
+    )
 }
