02b9d6befef8a56e984905eb66cea6780267686e
alexbelz123 <belz.alexx@gmail.com>
2023-07-05 15:23:17 +0300

    MAV-1819 #time 9m  /fixed type conflicts after merge

--- src/frontend/src/providers/UserProvider/user.provider.types.ts
+++ src/frontend/src/providers/UserProvider/user.provider.types.ts
@@ -1,4 +1,5 @@
 import { TokenAddressType, UserMTokenType } from 'providers/TokensProvider/tokens.provider.types'
+import { GetUserDataSubscription } from 'utils/__generated__/graphql'
 import { z } from 'zod'
 import { CLAIM_ALL_REWARDS_ACTION, CLAIM_VESTING_REWARD_ACTION } from './helpers/user.consts'
 
@@ -45,20 +46,6 @@ export type UserTzktAccountType = z.infer<typeof userTzktWSAccountSchema>
 export const userTzktWSAccountSchema = z.array(userTzktAccountSchema)
 export type UserTzktWSAccountType = z.infer<typeof userTzktWSAccountSchema>
 
-export interface UserFarmRewardsData {
-  generalAccumulatedRewardsPerShare: number
-  currentRewardPerBlock: number
-  lastBlockUpdate: number
-  generalTotalRewards: number
-  generalPaidReward: number
-  generalUnpaidReward: number
-  infinite: boolean
-  totalLPTokenDeposited: number
-  myDepositedAmount: number
-  myParticipationRewardsPerShare: number
-  myAvailableFarmRewards: number
-}
-
 // Context types
 export type UserContext = {
   // user's metadata
@@ -89,7 +76,7 @@ export type UserContext = {
   availableDoormanRewards: number
   availableSatellitesRewards: number
   availableLoansRewards: number
-  availableFarmRewards: Record<string, UserFarmRewardsData>
+  availableFarmRewards: Record<string, number>
 
   // user tokens
   userTokensBalances: Record<TokenAddressType, number>
@@ -103,8 +90,13 @@ export type UserContext = {
   changeUser: () => void
 }
 
+export type UserIndexerFarmRewardsType = GetUserDataSubscription['mavryk_user'][number]['farm_accounts']
+
 export type UserContextStateType = UserMetadataType &
-  Pick<UserContext, 'userTokensBalances' | 'userMTokens' | 'userAddress' | 'availableLoansRewards'> & {
+  Pick<
+    UserContext,
+    'userTokensBalances' | 'userMTokens' | 'userAddress' | 'availableLoansRewards' | 'availableFarmRewards'
+  > & {
     farmAccounts: UserIndexerFarmRewardsType
   }
 
@@ -121,7 +113,6 @@ export type UserMetadataType = Pick<
   | 'gatheredSatellitesRewards'
   | 'gatheredDoormanRewards'
   | 'availableDoormanRewards'
-  | 'availableFarmRewards'
   | 'availableSatellitesRewards'
 > & {
   farmAccounts: UserIndexerFarmRewardsType
