ba64a5b60df2ecbc1e8de99d9bad1abb14479afd
Evgeniy Polishchuk <evgeniy.polishchuk.memcrab@gmail.com>
2023-07-07 14:36:08 +0300

    MAV-1846 #time 14m updated colors for sliding tab buttons component

--- src/frontend/src/app/App.components/SlidingTabButtons/SlidingTabButtons.style.tsx
+++ src/frontend/src/app/App.components/SlidingTabButtons/SlidingTabButtons.style.tsx
@@ -1,13 +1,10 @@
 import styled, { css } from 'styled-components/macro'
-import { turn } from 'styles/animations'
-import { darkColor } from '../../../styles'
 import { BUTTON_RADIUS } from '../../../styles/constants'
 import { MavrykTheme } from '../../../styles/interfaces'
-import { PRIMARY, SECONDARY, TRANSPARENT } from './SlidingTabButtons.constants'
 
 export const SlidingTabButtonsStyled = styled.div<{ theme: MavrykTheme }>`
-  background-color: ${darkColor};
-  border: 1px solid ${({ theme }) => theme.lPurple_dPurple_lPuprple};
+  background-color: ${({ theme }) => theme.backgroundColor};
+  border: 1px solid ${({ theme }) => theme.strokeColor};
   display: flex;
   justify-content: space-evenly;
   align-items: center;
@@ -16,7 +13,7 @@ export const SlidingTabButtonsStyled = styled.div<{ theme: MavrykTheme }>`
   padding: 1px;
 
   &.disabled {
-    opacity: 0.7;
+    opacity: 0.6;
     cursor: not-allowed;
 
     > button {
@@ -43,12 +40,12 @@ export const ButtonStyled = styled.button<{ disabled: boolean; theme: MavrykThem
   padding: 0 22px;
   border-radius: ${BUTTON_RADIUS};
   user-select: none;
-  color: ${({ theme }) => theme.lightTextColor};
+  color: ${({ theme }) => theme.regularText};
   background: transparent;
 
   &.selected {
-    color: ${({ theme }) => theme.containerColor};
-    background: linear-gradient(90deg, #86d4c9 0.31%, #8d86eb 99.97%);
+    color: ${({ theme }) => theme.backgroundColor};
+    background: ${({ theme }) => theme.forTabs};
   }
 
   &.loading {
@@ -75,41 +72,5 @@ export const ButtonText = styled.div<{ theme: MavrykTheme }>`
     font-weight: 600;
     font-size: 14px;
     line-height: 21px;
-  }
-  &.${PRIMARY} {
-    color: ${({ theme }) => theme.textColor};
-  }
-
-  &.${SECONDARY} {
-    color: ${({ theme }) => theme.primaryColor};
-  }
-
-  &.${TRANSPARENT} {
-    color: ${({ theme }) => theme.primaryColor};
-  }
-`
-
-export const ButtonLoadingIcon = styled.svg<{ theme: MavrykTheme }>`
-  width: 16px;
-  height: 16px;
-  margin-top: 4px;
-  margin-right: 15px;
-  vertical-align: sub;
-  stroke: ${({ theme }) => theme.textColor};
-  stroke-width: 1px;
-  stroke-dashoffset: 94.248;
-  stroke-dasharray: 47.124;
-  animation: ${turn} 1.6s linear infinite forwards;
-
-  &.${PRIMARY} {
-    stroke: ${({ theme }) => theme.containerColor};
-  }
-
-  &.${SECONDARY} {
-    stroke: ${({ theme }) => theme.primaryColor};
-  }
-
-  &.${TRANSPARENT} {
-    stroke: ${({ theme }) => theme.textColor};
   }
 `
