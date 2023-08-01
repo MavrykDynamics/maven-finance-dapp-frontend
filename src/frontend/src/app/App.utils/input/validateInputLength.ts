a9b5ac112822d6e6040796055bcd16105ee00a60
alexbelz123 <belz.alexx@gmail.com>
2023-08-01 11:22:13 +0300

    MAV-2045 #time 1m  /removed file

--- src/frontend/src/app/App.utils/input/validateInputLength.ts
+++
@@ -1,8 +0,0 @@
-import { defaultLargeInputMaxLength } from 'app/App.components/Input/Input.constants'
-import { INPUT_LIMIT_TEXT } from './input.consts'
-
-export function validateInputLength(value: string, limit = defaultLargeInputMaxLength) {
-  if (value.length > limit) return INPUT_LIMIT_TEXT
-
-  return ''
-}
