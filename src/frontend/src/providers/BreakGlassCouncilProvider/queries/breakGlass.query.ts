4cb86f142789a1762d6ddaa7cc540e8765f6b3e3
alexbelz123 <belz.alexx@gmail.com>
2023-09-08 12:18:38 +0300

    MAV-1994 #time 22m  /fixed wrong query

--- src/frontend/src/providers/BreakGlassCouncilProvider/queries/breakGlass.query.ts
+++ src/frontend/src/providers/BreakGlassCouncilProvider/queries/breakGlass.query.ts
@@ -6,7 +6,9 @@ import {
   ALL_BG_ONGOING_COUNCIL_ACTIONS_SUB,
   ALL_BG_PAST_COUNCIL_ACTIONS_SUB,
   MY_BG_ONGOING_COUNCIL_ACTIONS_SUB,
+  MY_BG_PAST_COUNCIL_ACTIONS_SUB,
 } from '../helpers/breakGlassCouncil.consts'
+import { ValidationError } from 'errors/error'
 
 export const BREAK_GLASS_COUNCIL_MEMBERS_QUERY = gql(`
   query GetBreakGlassCouncilMembers {
@@ -25,24 +27,31 @@ export const BREAK_GLASS_COUNCIL_MEMBERS_QUERY = gql(`
   }
 `)
 
-function getBreakGlassCouncilActionsFilter(actionType: BreakGlassCouncilActionsSubsType, userAddress: string) {
+function getBreakGlassCouncilActionsFilter(actionType: BreakGlassCouncilActionsSubsType) {
   switch (actionType) {
     case ALL_BG_PAST_COUNCIL_ACTIONS_SUB:
-      return 'expiration_datetime: {_lt: $currentTimestamp}, _or: {executed: {_eq: true}'
+      return 'expiration_datetime: {_lt: $currentTimestamp}, _or: {executed: {_eq: true}}'
     case ALL_BG_ONGOING_COUNCIL_ACTIONS_SUB:
-      return 'expiration_datetime: {_gt: $currentTimestamp}, _or: {executed: {_eq: false}'
+      return 'expiration_datetime: {_gt: $currentTimestamp}, _or: {executed: {_eq: false}}'
+    case MY_BG_ONGOING_COUNCIL_ACTIONS_SUB:
+      return 'expiration_datetime: {_gt: $currentTimestamp}, _or: {executed: {_eq: false}, initiator: {address: {_eq: $userAddress}}}'
+    case MY_BG_PAST_COUNCIL_ACTIONS_SUB:
+      return 'expiration_datetime: {_lt: $currentTimestamp}, _or: {executed: {_eq: true}, initiator: {address: {_eq: $userAddress}}}'
+    default:
+      throw new ValidationError('Break Glass actions filter error', {
+        code: 400,
+      })
   }
 }
 
 export const getBreakGlassCouncilActions = (
   actionType: BreakGlassCouncilActionsSubsType,
-  userAddress: string,
 ): DocumentNode | TypedDocumentNode<GetPastBreakGlassCouncilActionsQuery, OperationVariables> => {
-  const filterCondition = getBreakGlassCouncilActionsFilter(actionType, userAddress)
+  const filterCondition = getBreakGlassCouncilActionsFilter(actionType)
 
   return apolloGql(`
-  query GetPastBreakGlassCouncilActions($currentTimestamp: timestamptz = '1970-01-01T00:00:00.000Z'){
-    break_glass_action(where: {${filterCondition}}, order_by: {start_datetime: desc}) {
+  query GetPastBreakGlassCouncilActions($currentTimestamp: timestamptz = "1970-01-01T00:00:00.000Z", $userAddress: String = ""){
+    break_glass_action(order_by: {start_datetime: desc}, where: {${filterCondition}}) {
       action_type
       break_glass {
         address
@@ -64,36 +73,3 @@ export const getBreakGlassCouncilActions = (
   }
   `)
 }
-
-// export const BREAK_GLASS_COUNCIL_MEMBERS_QUERY_NAME = 'GetBreakGlassCouncilMemberQuery'
-// export const BREAK_GLASS_COUNCIL_MEMBERS_QUERY_VARIABLE = {}
-
-// const BREAK_GLASS_ACTIONS_PARAMS = `
-
-// `
-
-// export const BREAK_GLASS_COUNCIL_PAST_ACTIONS_QUERY = `
-//   query GetPastBreakGlassCouncilActions($_lt: timestamptz = "") {
-//     break_glass_action(where: {expiration_datetime: {_lt: $_lt}, _or: {executed: {_eq: true}}}, order_by: {start_datetime: desc}) {
-//      ${BREAK_GLASS_ACTIONS_PARAMS}
-//     }
-//   }
-// `
-
-// export const BREAK_GLASS_COUNCIL_PAST_ACTIONS_QUERY_NAME = 'GetPastBreakGlassCouncilActions'
-// export function BREAK_GLASS_COUNCIL_PAST_ACTIONS_QUERY_VARIABLE(variables: { _lt?: string }) {
-//   return variables
-// }
-
-// export const BREAK_GLASS_COUNCIL_PENDING_ACTIONS_QUERY = `
-//   query GetPendingBreakGlassCouncilActions($_gte: timestamptz = "", $userAddress: String = "", $userAddress2: String = "") {
-//     break_glass_action(where: {expiration_datetime: {_gte: $_gte}, _or: {executed: {_eq: false}}, initiator: {address: {_neq: $userAddress}}, signers: {signer: {address: {_neq: $userAddress2}}}}, order_by: {start_datetime: desc}) {
-//       ${BREAK_GLASS_ACTIONS_PARAMS}
-//     }
-//   }
-// `
-
-// export const BREAK_GLASS_COUNCIL_PENDING_ACTIONS_QUERY_NAME = 'GetPendingBreakGlassCouncilActions'
-// export function BREAK_GLASS_COUNCIL_PENDING_ACTIONS_QUERY_VARIABLE(variables: { _gte?: string }) {
-//   return variables
-// }
