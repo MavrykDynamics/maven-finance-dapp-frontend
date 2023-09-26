bcc2a5e17759885e261299a9e098e6aac370efe4
kucher-mx <max.kucher98@gmail.com>
2023-09-26 11:43:14 +0300

    MAV-1806 #time 32m   added 404 permanent

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -87,7 +87,8 @@ const DappSectionsDataProviders = ({ children }: { children: React.ReactNode }) 
   // use user loading status only on dapp init loading
   const isInitialUserLoading = !isUserRestored ? isUserLoading : false
 
-  const isInitialLoading = isDappGeneralLoading || isTokensLoading || isFeedsLoading || isInitialUserLoading
+  // TODO: reset to comment when api will be ok
+  const isInitialLoading = false //isDappGeneralLoading || isTokensLoading || isFeedsLoading || isInitialUserLoading
 
   return (
     <>
