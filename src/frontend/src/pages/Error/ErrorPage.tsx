4d1403214fe38ca70bf4f37e88bcb3f7b7bbf292
kucher-mx <max.kucher98@gmail.com>
2023-10-02 13:22:55 +0300

    MAV-2205 #time 5m   restored back to dashboard btn on wrong route

--- src/frontend/src/pages/Error/ErrorPage.tsx
+++ src/frontend/src/pages/Error/ErrorPage.tsx
@@ -61,13 +61,12 @@ export const ErrorPage = ({
             <Vector2 src={`/images/${themeSelected}/space-cow.svg`} />
             <ErrorMidHeader>{headerText}</ErrorMidHeader>
             <ErrorParagraph>{descText}</ErrorParagraph>
-            {/* TODO: remove after api update */}
-            {/* {type === ERROR_TYPE_ROUTER && (
+            {type === ERROR_TYPE_ROUTER && (
               <NewButton kind={BUTTON_PRIMARY} onClick={handleRedirect}>
                 <Icon id="menu-compass" />
                 Go To Dashboard
               </NewButton>
-            )} */}
+            )}
           </ErrorPageInner>
           <ErrorFooter themeSelected={themeSelected} handleRedirect={handleRedirect} />
         </ErrorPageWrapper>
