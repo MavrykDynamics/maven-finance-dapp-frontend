a48327265b83cbea6852e3188a1aaf28876a9fe6
kucher-mx <max.kucher98@gmail.com>
2023-10-16 17:51:47 +0300

    MAV-2261 #time 14m   fixes for eGov

--- src/frontend/src/providers/EmergencyGovernanceProvider/queries/eGovConfig.query.ts
+++ src/frontend/src/providers/EmergencyGovernanceProvider/queries/eGovConfig.query.ts
@@ -2,7 +2,7 @@ import { gql } from 'utils/__generated__'
 
 export const EGOV_CONFIG_QUERY = gql(`
 	query GetEGovConfigQuery {
-		emergency_governance {
+		emergency_governance: emergency_governance {
 			current_emergency_record_id
 			smvk_percentage_required
 			required_fee_mutez
