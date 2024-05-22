6f38dadaf22c5a7d6dfbdd1d4428c9525a937811
kucher-mx <max.kucher98@gmail.com>
2024-05-22 16:51:43 +0300

    removed log and updated restore user cond

--- src/frontend/src/pages/DataFeeds/DataFeeds.controller.tsx
+++ src/frontend/src/pages/DataFeeds/DataFeeds.controller.tsx
@@ -57,8 +57,6 @@ export const DataFeeds = () => {
       feedsAddresses.filter((feedAddress) => {
         const { category, name, address } = feedsMapper[feedAddress]
 
-        console.log({ feed: feedsMapper[feedAddress] })
-
         if (chosenDdItem.toLowerCase() === 'all') {
           return (
             name.toLowerCase().includes(searchInputValue.toLowerCase()) ||
