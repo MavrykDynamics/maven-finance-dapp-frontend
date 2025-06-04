b7f8283267d4cfbf6f29ba419dc03b06c094424b
alexbelz123 <belz.alexx@gmail.com>
2025-06-04 16:41:31 +0300

    feed details oracle data

--- src/frontend/src/pages/DataFeedsDetails/DataFeedsDetails.controller.tsx
+++ src/frontend/src/pages/DataFeedsDetails/DataFeedsDetails.controller.tsx
@@ -43,6 +43,7 @@ import { DataLoaderWrapper, SpinnerCircleLoaderStyled } from 'app/App.components
 import {
   DEFAULT_SATELLITES_ACTIVE_SUBS,
   SATELLITE_DATA_SUB,
+  SATELLITE_PAGINATION_ORACLES,
   SATELLITES_DATA_ORACLES_SUB,
 } from 'providers/SatellitesProvider/satellites.const'
 import { ACTION_PRIMARY, ACTION_SIMPLE } from 'app/App.components/Button/Button.constants'
@@ -89,9 +90,12 @@ const DataFeedDetails = () => {
 
   const {
     oraclesIds,
-    satelliteMapper,
+    satelliteOraclesMapper,
+    oracleSatellitesCount,
     isLoading: isSatellitesLoading,
     changeSatellitesSubscriptionsList,
+    updateSatelliteQueryFilters,
+    changePage,
   } = useSatellitesContext()
   const [chartPeriod, setChartPeriod] = useState<ChartPeriodType>(ONE_HOUR)
   const { isLoading: isFeedsChartsLoading, dataFeedsHistory, dataFeedsVolatility } = useFeedCharts(feedId, chartPeriod)
@@ -107,6 +111,19 @@ const DataFeedDetails = () => {
   }, [])
 
   const feed = feedsMapper[feedId]
+
+  const feedOracleAddresses = useMemo(() => feed.oraclesAddresses.map((address) => address), [feed.oraclesAddresses])
+
+  useEffect(() => {
+    if (feedOracleAddresses.length > 0) {
+      const oracleParamsByFeed = { where: { user: { address: { _in: feedOracleAddresses } } } }
+      updateSatelliteQueryFilters(oracleParamsByFeed, SATELLITE_PAGINATION_ORACLES)
+    }
+
+    return () => {
+      updateSatelliteQueryFilters({ where: {} }, SATELLITE_PAGINATION_ORACLES)
+    }
+  }, [feedOracleAddresses])
 
   // Before trusted wias checked like this feed && feed.oraclesResponces >= feed.pct_oracle_threshold
   const [isTrustedAnswer, setTrustedAnswer] = useState(true)
@@ -134,22 +151,12 @@ const DataFeedDetails = () => {
   })
 
   const chartPlots = (activeTab === 1 ? dataFeedsHistory : dataFeedsVolatility) ?? []
+  const currentPage = useMemo(() => getPageNumber(search, ORACLES_DATA_IN_FEED_LIST_NAME), [search])
 
-  const feedsSatellites = useMemo(
-    () =>
-      feedId
-        ? oraclesIds
-            .filter((address) => satelliteMapper[address].participatedFeeds[feedId])
-            .map((address) => satelliteMapper[address])
-        : [],
-    [feedId, oraclesIds, satelliteMapper],
-  )
-
-  const paginatedFeedsOracles = useMemo(() => {
-    const currentPage = getPageNumber(search, ORACLES_DATA_IN_FEED_LIST_NAME)
-    const [from, to] = calculateSlicePositions(currentPage, ORACLES_DATA_IN_FEED_LIST_NAME)
-    return feedsSatellites.slice(from, to)
-  }, [feedsSatellites, search])
+  console.log('render')
+  useEffect(() => {
+    changePage(currentPage, SATELLITE_PAGINATION_ORACLES)
+  }, [currentPage])
 
   const chartDataAverage = chartPlots.reduce((acc, { value }) => (acc += value), 0) / chartPlots.length
 
@@ -403,14 +410,14 @@ const DataFeedDetails = () => {
                 <SpinnerCircleLoaderStyled className={SPINNER_LOADER_LARGE} />
                 <div className="text">Loading oracles data</div>
               </DataLoaderWrapper>
-            ) : paginatedFeedsOracles.length ? (
+            ) : oraclesIds.length ? (
               <div className={`oracles-list`}>
-                {feedsSatellites.map((item) => (
-                  <OracleCard oracle={item} key={item.address} feed={feed} />
+                {oraclesIds.map((address) => (
+                  <OracleCard oracle={satelliteOraclesMapper[address]} key={address} feed={feed} />
                 ))}
 
                 <Pagination
-                  itemsCount={feedsSatellites.length}
+                  itemsCount={oracleSatellitesCount}
                   side={PAGINATION_SIDE_RIGHT}
                   listName={ORACLES_DATA_IN_FEED_LIST_NAME}
                 />
