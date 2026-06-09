e5dd42a540381925be961a76646c9145637ea8cc
alexbelz123 <belz.alexx@gmail.com>
2026-06-09 09:57:33 +0300

    remoced tzkt mentions in footer

--- src/frontend/src/app/App.components/Footer/Footer.tsx
+++ src/frontend/src/app/App.components/Footer/Footer.tsx
@@ -4,17 +4,6 @@ import { mavrykLinks } from './footer.const'
 export const Footer = () => {
   return (
     <FooterStyled>
-      <div className="powered-by">
-        Powered by&nbsp;
-        <a href="https://tzkt.io" target="_blank" rel="noreferrer">
-          TzKT API
-        </a>
-        &nbsp; & &nbsp;
-        <a href="https://dipdup.io" target="_blank" rel="noreferrer">
-          DipDup
-        </a>
-      </div>
-
       <div className="additional-links">
         {mavrykLinks.map(({ link, title }) => (
           <a key={link} href={link} target="_blank" rel="noreferrer">
